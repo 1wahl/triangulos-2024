8ed21a134158f340c0e0243f11c80c6c1c9a7ce6
1wahl <161186106+1wahl@users.noreply.github.com>
2024-05-17 22:08:24 -0300

    Add files via upload

--- src/Triangulos.ConsoleApp/Program.cs
+++ src/Triangulos.ConsoleApp/Program.cs
@@ -1,10 +1,76 @@
 ﻿namespace Triangulos.ConsoleApp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            char opcao = 'S';
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("-- Verificação de triângulos --,");
+
+                Console.WriteLine("digite o primeiro segmento: ");
+                Triangulos.x = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("digite o segundo segmento: ");
+                Triangulos.y = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("digite o terceiro segmento: ");
+                Triangulos.z = Convert.ToInt32(Console.ReadLine());
+
+
+
+                if (Triangulos.TrianguloPodeSerCriado(Triangulos.x, Triangulos.y, Triangulos.z) == false)
+                {
+                    Console.WriteLine("Por favor digite uma opção válida");
+                }
+
+                Triangulos.TipoTriangulo(Triangulos.x, Triangulos.y, Triangulos.z);
+
+                Console.WriteLine("Deseja continuar (S/N)");
+                opcao = char.Parse(Console.ReadLine());
+
+            } while (opcao != 'N' || opcao != 'n');
+        }
+    }
+
+    public static class Triangulos
+    {
+        public static double x;
+        public static double y;
+        public static double z;
+
+        public static bool TrianguloPodeSerCriado(double x, double y, double z)
+        {
+            if (x < y + z && y < x + z && z < x + y)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
+        }
+
+        public static void TipoTriangulo(double x, double y, double z)
+        {
+            Console.Write("Triângulo ");
+
+            if (x != y && y != z && z != x)
+            {
+                Console.WriteLine("Escaleno");
+            }
+            else if (x == y && y == z && z == x)
+            {
+                Console.WriteLine("Equilátero");
+            }
+            else if (x == y || y == z || z == x)
+            {
+                Console.WriteLine("Isósceles");
+            }
+            
         }
     }
 }
